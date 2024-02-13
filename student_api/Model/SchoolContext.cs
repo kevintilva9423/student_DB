f800360aa797882fc6ebfbc6901949f7a0f6354b
kevintilva9423 <kevintilva12345@gmail.com>
2024-02-13 19:02:12 +0530

    all project commit

---
+++ student_api/Model/SchoolContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace student_api.Model
+{
+    public class SchoolContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+
+        public SchoolContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+    }
+}
---
+++ student_api/Model/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace student_api.Model
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime EnrollmentDate { get; set; }
+    }
+}
